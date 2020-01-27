620bb135c67c68eca475669a564b8c05a2c81f0e
lesley <lesleybanadzem2017@gmail.com>
2020-01-27 11:36:36 -0800

    Created a simple / endpoint to test the app works just fine

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(helmet())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req,res, next) => {
+    res.status(200).send('Welcome to the project manager')
+})
 
 app.listen(PORT, console.log(`App listening to http://localhost:${PORT}`))
 
